6ff379c9748509cfa1e0f090ab080064d72a7bcc
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-12-29 13:02:18 +0600

    seekbar learning

--- app/src/main/java/com/example/widgetskotlin/MainActivity.kt
+++ app/src/main/java/com/example/widgetskotlin/MainActivity.kt
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
+// creating objects to interact with the ui
     private lateinit var radioGroup: RadioGroup
     private  lateinit var radioButton: RadioButton
+    private lateinit var seekBar: SeekBar
+    private lateinit var textViewProgress: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+//finding views by id
         radioGroup = findViewById(R.id.radioGroup)
         radioGroup.setOnCheckedChangeListener { radioGroup, id ->
             radioButton = findViewById(id)
@@ -29,6 +34,28 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this@MainActivity, "Maybe button clicked", Toast.LENGTH_SHORT).show()
                 }
             }
+
         }
+//
+        seekBar = findViewById(R.id.seekBar)
+        textViewProgress = findViewById(R.id.texts)
+
+        seekBar.setOnSeekBarChangeListener(object: OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                Toast.makeText(this@MainActivity, "on progress change", Toast.LENGTH_SHORT).show()
+                textViewProgress.text = "Rate =" + seekBar.progress.toString()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                Toast.makeText(this@MainActivity, "on start tracking touch", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                Toast.makeText(this@MainActivity, "on stop tracking touch", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+
     }
 }
